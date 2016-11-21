75c07188674c773de6935fb720f9a068f2f44009
Voidmenull <thehedgehog@arcor.de>
2016-11-21 22:47:58 +0100

    another tooltip fix

--- EzPoison.lua
+++ EzPoison.lua
@@ -39,8 +39,8 @@ EZP.Work = 	{
 	PoisonID = {
 		[1] = {6947,6949,6950,8926,8927,8928},
 		[2] = {2892,2893,8984,8985,20844},
-		[3] = {3775},
-		[4] = {3776},
+		[3] = 3775,
+		[4] = 3776,
 		[5] = {5237,6951,9186},
 		[6] = {10918,10920,10921,10922},
 	},
@@ -646,7 +646,7 @@ function EZP:GetInventoryID(hand)
 			for i=0,4 do -- i = bagsnr.
 				for j=1,18 do -- j = slotnr.
 					if GetContainerItemInfo(i, j) and H <= 7 and H >= 2 then
-						if gsub(GetContainerItemLink(i,j),"^.*%[(.*)%].*$","%1") == EZP.Work.Poison[H-1] then return {i,j,"",EZP.Work.PoisonID[3][1],H-1} end
+						if gsub(GetContainerItemLink(i,j),"^.*%[(.*)%].*$","%1") == EZP.Work.Poison[H-1] then return {i,j,"",EZP.Work.PoisonID[3],H-1} end
 					end
 				end
 			end
@@ -657,7 +657,7 @@ function EZP:GetInventoryID(hand)
 			for i=0,4 do -- i = bagsnr.
 				for j=1,18 do -- j = slotnr.
 					if GetContainerItemInfo(i, j) and H <= 7 and H >= 2 then
-						if gsub(GetContainerItemLink(i,j),"^.*%[(.*)%].*$","%1") == EZP.Work.Poison[H-1].." II" then return {i,j,"",EZP.Work.PoisonID[4][1],H-1} end
+						if gsub(GetContainerItemLink(i,j),"^.*%[(.*)%].*$","%1") == EZP.Work.Poison[H-1].." II" then return {i,j,"",EZP.Work.PoisonID[4],H-1} end
 					end
 				end
 			end
